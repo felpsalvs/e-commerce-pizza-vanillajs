09caa5a5310e4f7765448b8da90c485f7ca62f10
Felipe Alves <profelipealves@outlook.com>
2022-08-06 23:01:55 -0300

    update

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-let modalQty = 1;
 let cart = [];
+let modalQt = 1;
 let modalKey = 0;
 
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
+// Listagem das pizzas
 pizzaJson.map((item, index) => {
   let pizzaItem = c(".models .pizza-item").cloneNode(true);
-  // preencher os dados da pizza
 
+  pizzaItem.setAttribute("data-key", index);
   pizzaItem.querySelector(".pizza-item--img img").src = item.img;
   pizzaItem.querySelector(
     ".pizza-item--price"
   ).innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
   pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
+
   pizzaItem.querySelector("a").addEventListener("click", (e) => {
     e.preventDefault();
     let key = e.target.closest(".pizza-item").getAttribute("data-key");
-    modalQty = 1;
+    modalQt = 1;
     modalKey = key;
 
-    // c(".pizzaBig img").src = pizzaJson[key].img;
     c(".pizzaBig img").src = pizzaJson[key].img;
     c(".pizzaInfo h1").innerHTML = pizzaJson[key].name;
     c(".pizzaInfo--desc").innerHTML = pizzaJson[key].description;
@@ -35,6 +36,7 @@ pizzaJson.map((item, index) => {
       }
       size.querySelector("span").innerHTML = pizzaJson[key].sizes[sizeIndex];
     });
+
     c(".pizzaInfo--qt").innerHTML = modalQt;
 
     c(".pizzaWindowArea").style.opacity = 0;
@@ -47,7 +49,7 @@ pizzaJson.map((item, index) => {
   c(".pizza-area").append(pizzaItem);
 });
 
-// Eventos do modal
+// Eventos do MODAL
 function closeModal() {
   c(".pizzaWindowArea").style.opacity = 0;
   setTimeout(() => {
@@ -60,7 +62,7 @@ cs(".pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton").forEach(
   }
 );
 c(".pizzaInfo--qtmenos").addEventListener("click", () => {
-  if (modalQty > 1) {
+  if (modalQt > 1) {
     modalQt--;
     c(".pizzaInfo--qt").innerHTML = modalQt;
   }
@@ -75,13 +77,9 @@ cs(".pizzaInfo--size").forEach((size, sizeIndex) => {
     size.classList.add("selected");
   });
 });
-
-// Eventos do carrinho
 c(".pizzaInfo--addButton").addEventListener("click", () => {
   let size = parseInt(c(".pizzaInfo--size.selected").getAttribute("data-key"));
-
-  let identifier = pizzaJson[modalKey].id + "_" + size;
-
+  let identifier = pizzaJson[modalKey].id + "@" + size;
   let key = cart.findIndex((item) => item.identifier == identifier);
   if (key > -1) {
     cart[key].qt += modalQt;
@@ -106,9 +104,8 @@ c(".menu-closer").addEventListener("click", () => {
   c("aside").style.left = "100vw";
 });
 
-// Atualizar carrinho
 function updateCart() {
-  c(".menu--openner span").innerHTML = cart.length;
+  c(".menu-openner span").innerHTML = cart.length;
 
   if (cart.length > 0) {
     c("aside").classList.add("show");
@@ -127,18 +124,19 @@ function updateCart() {
       let pizzaSizeName;
       switch (cart[i].size) {
         case 0:
-          pizzaSizeName = "Pequena";
+          pizzaSizeName = "P";
           break;
         case 1:
-          pizzaSizeName = "Média";
+          pizzaSizeName = "M";
           break;
         case 2:
-          pizzaSizeName = "Grande";
+          pizzaSizeName = "G";
           break;
       }
       let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
       cartItem.querySelector("img").src = pizzaItem.img;
-      cartItem.querySelector(".cart--item-name").innerHTML = pizzaName;
+      cartItem.querySelector(".cart--item-nome").innerHTML = pizzaName;
       cartItem.querySelector(".cart--item--qt").innerHTML = cart[i].qt;
       cartItem
         .querySelector(".cart--item-qtmenos")
@@ -157,15 +155,17 @@ function updateCart() {
           updateCart();
         });
 
-      c("cart").append(cartItem);
+      c(".cart").append(cartItem);
     }
+
     desconto = subtotal * 0.1;
     total = subtotal - desconto;
+
     c(".subtotal span:last-child").innerHTML = `R$ ${subtotal.toFixed(2)}`;
     c(".desconto span:last-child").innerHTML = `R$ ${desconto.toFixed(2)}`;
     c(".total span:last-child").innerHTML = `R$ ${total.toFixed(2)}`;
   } else {
     c("aside").classList.remove("show");
-    c('aside').style.left = "100vw";
+    c("aside").style.left = "100vw";
   }
 }
